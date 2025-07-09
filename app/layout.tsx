240b63d3a128e0068c6c8d4b41c8ddf8bf5ddaa2
saadmuk123 <amir.saad@aun.edu.ng>
2025-07-09 22:30:01 +0100

    First-Push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+//                Next.js tutorial
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
