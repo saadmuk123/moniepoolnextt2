9eb4555fa73319134a0a72328ac7117234c7bc99
saadmuk123 <amir.saad@aun.edu.ng>
2025-07-13 10:55:47 +0100

    Dashboard add and Invoice

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-        src="/hero-desktop.png"
+        src="/falling-money-49095.png"
         width={1000}
         height={760}
         className="hidden md:block"
